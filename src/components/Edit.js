757997a9d591858dc2f1d2b519516b03565071cd
waldemarciupa <waldemarciupa90@gmail.com>
2022-03-24 15:35:33 +0100

    create section component

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -14,6 +14,7 @@ import Label from './Label';
 import Input from './Input';
 import CustomFileInput from './CustomFileInput';
 import EditField from './EditField';
+import Section from './Section';
 
 const Container = styled.div`
   width: 50%;
@@ -21,40 +22,10 @@ const Container = styled.div`
   overflow: auto;
 `;
 
-const Heading = styled.h2`
-  font-size: 18px;
-  line-height: 26px;
-  font-weight: 700;
-  margin: 20px 0 2px;
-`;
-
-const Description = styled.p`
-  margin-top: 2px;
-  font-size: 14px;
-  line-height: 20px;
-  color: #737373;
-`;
-
 const InputWrapper = styled.div`
   width: 100%;
   display: ${(props) => props.display};
   gap: ${(props) => props.gap};
-`;
-
-const Button = styled.button`
-  width: 100%;
-  height: 40px;
-  border: none;
-  border-radius: 4px;
-  display: grid;
-  place-content: center;
-  background-color: #f4f4f5;
-  cursor: pointer;
-  transition: background-color 0.1s linear;
-
-  &:hover {
-    background-color: rgba(57, 76, 96, 0.15);
-  }
 `;
 
 const Edit = () => {
@@ -64,116 +35,103 @@ const Edit = () => {
   return (
     <Container>
       <form>
-        <Heading>Personal details</Heading>
-        <Description>
-          In this section you can enter your personal data, not all fields are
-          required.
-        </Description>
-        <InputWrapper display='flex' gap='28px'>
-          <CustomFileInput />
-          <InputWrapper>
-            <InputWrapper display='flex' gap='28px'>
-              <InputWrapper>
-                <Label>First name</Label>
-                <Input
-                  placeholder='Enter your name'
-                  value={profile.name}
-                  handler={(e) => {
-                    dispatch(setName(e.target.value));
-                  }}
-                />
-              </InputWrapper>
-              <InputWrapper>
-                <Label>Last name: </Label>
-                <Input
-                  placeholder='Enter your surname'
-                  value={profile.surname}
-                  handler={(e) => {
-                    dispatch(setSurname(e.target.value));
-                  }}
-                />
-              </InputWrapper>
-            </InputWrapper>
+        <Section
+          heading={'Personal details'}
+          description={` In this section you can enter your personal data, not all fields are
+          required.`}
+        >
+          <InputWrapper display='flex' gap='28px'>
+            <CustomFileInput />
             <InputWrapper>
-              <Label>Position: </Label>
-              <Input
-                placeholder='Enter your wanted or current job position'
-                value={profile.position}
-                handler={(e) => {
-                  dispatch(setPosition(e.target.value));
-                }}
-              />
+              <InputWrapper display='flex' gap='28px'>
+                <InputWrapper>
+                  <Label>First name</Label>
+                  <Input
+                    placeholder='Enter your name'
+                    value={profile.name}
+                    handler={(e) => {
+                      dispatch(setName(e.target.value));
+                    }}
+                  />
+                </InputWrapper>
+                <InputWrapper>
+                  <Label>Last name: </Label>
+                  <Input
+                    placeholder='Enter your surname'
+                    value={profile.surname}
+                    handler={(e) => {
+                      dispatch(setSurname(e.target.value));
+                    }}
+                  />
+                </InputWrapper>
+              </InputWrapper>
+              <InputWrapper>
+                <Label>Position: </Label>
+                <Input
+                  placeholder='Enter your wanted or current job position'
+                  value={profile.position}
+                  handler={(e) => {
+                    dispatch(setPosition(e.target.value));
+                  }}
+                />
+              </InputWrapper>
             </InputWrapper>
           </InputWrapper>
-        </InputWrapper>
-        <InputWrapper>
-          <Label>Email: </Label>
-          <Input
-            placeholder='Enter your email'
-            value={profile.email}
-            handler={(e) => {
-              dispatch(setEmail(e.target.value));
-            }}
-          />
-        </InputWrapper>
-        <InputWrapper>
-          <Label>Phone: </Label>
-          <Input
-            placeholder='Enter your phone number'
-            value={profile.phone}
-            handler={(e) => {
-              dispatch(setPhone(e.target.value));
-            }}
-          />
-        </InputWrapper>
-        <InputWrapper>
-          <Label>Location: </Label>
-          <Input
-            placeholder='Enter your location'
-            value={profile.location}
-            handler={(e) => {
-              dispatch(setLocation(e.target.value));
-            }}
-          />
-        </InputWrapper>
-        <div>
-          <Heading>Employment history</Heading>
-          <Description>
-            Here you can show your employment history, don't hesitate to include
-            the description, but it's not required.
-          </Description>
+          <InputWrapper>
+            <Label>Email: </Label>
+            <Input
+              placeholder='Enter your email'
+              value={profile.email}
+              handler={(e) => {
+                dispatch(setEmail(e.target.value));
+              }}
+            />
+          </InputWrapper>
+          <InputWrapper>
+            <Label>Phone: </Label>
+            <Input
+              placeholder='Enter your phone number'
+              value={profile.phone}
+              handler={(e) => {
+                dispatch(setPhone(e.target.value));
+              }}
+            />
+          </InputWrapper>
+          <InputWrapper>
+            <Label>Location: </Label>
+            <Input
+              placeholder='Enter your location'
+              value={profile.location}
+              handler={(e) => {
+                dispatch(setLocation(e.target.value));
+              }}
+            />
+          </InputWrapper>
+        </Section>
+        <Section
+          heading={'Employment history'}
+          description={`Here you can show your employment history, don't hesitate to include
+            the description, but it's not required.`}
+          clickHandler={addEmployment}
+          btnText={'Add employment'}
+        >
           {profile.employments.length > 0 &&
             profile.employments.map((employment) => (
               <EditField key={employment.id} employment={employment} />
             ))}
-          <Button
-            type='button'
-            onClick={() => {
-              dispatch(addEmployment());
-            }}
-          >
-            Add employment
-          </Button>
-        </div>
-        <div>
-          <Heading>Skills</Heading>
-          <Description>
-            You can add the most important skills you know. You don't need to
-            specify your skill level.
-          </Description>
+        </Section>
+        <Section
+          heading={'Skills'}
+          description={`You can add the most important skills you know. You don't need to
+          specify your skill level.`}
+          clickHandler={addSkill}
+          btnText={'Add skill'}
+        >
           {profile.skills.length > 0 &&
             profile.skills.map((skill) => (
               <EditField key={skill.id} skill={skill} />
             ))}
-          <Button
-            type='button'
-            onClick={() => {
-              dispatch(addSkill());
-            }}
-          >
-            Add skill
-          </Button>
-        </div>
+        </Section>
       </form>
     </Container>
   );
