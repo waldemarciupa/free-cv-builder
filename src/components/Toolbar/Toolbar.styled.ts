9b61d5296dab47735a3bcd90ace6b26a92664663
waldemarciupa <waldemarciupa90@gmail.com>
2022-05-03 18:12:20 +0200

    create toolbar styled

---
+++ src/components/Toolbar/Toolbar.styled.ts
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+import { PDFDownloadLink } from '@react-pdf/renderer';
+
+const Container = styled.div`
+  width: 100%;
+  background: #303031;
+  padding: 12px 48px;
+  display: grid;
+  place-content: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Logo = styled.h1`
+  margin: 0;
+  padding: 0;
+  line-height: 14px;
+  font-size: 14px;
+`;
+
+const Link = styled.a`
+  color: #fff;
+  text-decoration: none;
+`;
+
+const DownloadLink = styled(PDFDownloadLink)`
+  font-size: 14px;
+  text-decoration: none;
+  line-height: 14px;
+  background-color: #303031;
+  color: #fff;
+  padding: 14px;
+  border-radius: 4px;
+  border: 1px solid #fff;
+`;
+
+export default { Container, Logo, Link, DownloadLink };
--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -1,61 +1,23 @@
-import styled from 'styled-components';
-import { PDFDownloadLink, StyleSheet } from '@react-pdf/renderer';
-import { Pdf } from '../View/Preview/Preview';
 import { useSelector } from 'react-redux';
+import { Pdf } from '../View/Preview/Preview';
 import { RootState } from '../../app/store';
-
-const Container = styled.div`
-  width: 100%;
-  background: #303031;
-  padding: 12px 48px;
-  display: grid;
-  place-content: center;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const Logo = styled.h1`
-  margin: 0;
-  padding: 0;
-  line-height: 14px;
-  font-size: 14px;
-`;
-
-const Link = styled.a`
-  color: #fff;
-  text-decoration: none;
-`;
-
-const styles = StyleSheet.create({
-  link: {
-    fontSize: '14px',
-    textDecoration: 'none',
-    lineHeight: '14px',
-    backgroundColor: '#303031',
-    color: '#fff',
-    padding: '14px',
-    borderRadius: '4px',
-    border: '1px solid #fff',
-  },
-});
+import Styled from './Toolbar.styled';
 
 const Toolbar = () => {
   const profile = useSelector((state: RootState) => state.profile);
 
   return (
-    <Container>
-      <Logo>
-        <Link href='/'>Free CV Builder</Link>
-      </Logo>
-      <PDFDownloadLink
-        style={styles.link}
+    <Styled.Container>
+      <Styled.Logo>
+        <Styled.Link href='/'>Free CV Builder</Styled.Link>
+      </Styled.Logo>
+      <Styled.DownloadLink
         document={<Pdf profile={profile} />}
         fileName='CV.pdf'
       >
         {({ loading }) => (loading ? 'Loading...' : 'Download PDF')}
-      </PDFDownloadLink>
-    </Container>
+      </Styled.DownloadLink>
+    </Styled.Container>
   );
 };
 
